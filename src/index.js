d614850794b94e3e3c945e8f79dcdaef7c05e109
William L <w-lindvall@outlook.com>
2020-01-09 15:22:16 -0800

    sketch initial frontend

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import VendingMachine from './vending-machine/index.js'
 
-const machine = new VendingMachine()
+const machine = new VendingMachine(
+  [{name: 'WATER', quantity: 2, price: 0.73}],
+  {
+    DOLLAR: 2,
+    TWO_DOLLAR: 1,
+    QUARTER: 1,
+  },
+)
 
-console.log(machine)
+const infoStock = document.getElementById('info-stock')
+const infoCoins = document.getElementById('info-coins')
+const formPurchase = document.getElementById('form-purchase')
+const inputPurchaseIndex = document.getElementById('input-index')
+
+const render = () => {
+  infoStock.innerText = JSON.stringify(machine.stock.get())
+  infoCoins.innerText = JSON.stringify(machine.coins.get())
+}
+
+formPurchase.addEventListener('submit', e => {
+  e.preventDefault()
+  machine.buy(inputPurchaseIndex.value, {TWO_DOLLAR: 1})
+  render()
+})
+
+render()
