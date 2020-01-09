3988c9b81a280f9ec62bfe01a1dc835c506f2eac
William L <w-lindvall@outlook.com>
2020-01-08 23:45:54 -0800

    transpile main script

--- index.js
+++
@@ -1,3 +0,0 @@
-import hello from './lib/vending-machine.js'
-
-console.log(hello)
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import VendingMachine from './vending-machine.js'
+
+const machine = new VendingMachine()
+console.log(machine.getStock())
