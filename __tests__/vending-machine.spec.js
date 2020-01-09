36c9b9b3942473ffda5ee802890cd1201492d661
William L <w-lindvall@outlook.com>
2020-01-09 13:26:45 -0800

    add initial purchase test

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -1,6 +1,30 @@
 import VendingMachine from '../lib/vending-machine/index.js'
 
 describe('VendingMachine', () => {
+  describe('buy(valid index, valid payment)', () => {
+    const machine = new VendingMachine(
+      [{name: 'WATER', quantity: 2, price: 0.73}],
+      {
+        DOLLAR: 2,
+        TWO_DOLLAR: 1,
+        QUARTER: 1,
+      },
+    )
+    const purchase = machine.buy(0, {TWO_DOLLAR: 1})
+
+    it('returns purchased item name', () => {
+      expect(purchase[0]).toEqual('WATER')
+    })
+    it('returns change in coins', () => {
+      expect(purchase[1]).toEqual({DOLLAR: 1, QUARTER: 1})
+    })
+    it('returns updated stock', () => {
+      expect(purchase[2]).toEqual([
+        {name: 'WATER', quantity: 1, price: 0.73},
+      ])
+    })
+  })
+
   describe('stock.get()', () => {
     it('returns empty array if stock empty', () => {
       expect(new VendingMachine().stock.get()).toEqual([])
@@ -10,11 +34,11 @@ describe('VendingMachine', () => {
   describe('stock.refill(valid quantity, valid index, no item obj)', () => {
     it('returns updated stock', () => {
       const machine = new VendingMachine([
-        {name: 'WATER', quantity: 2, price: 10},
+        {name: 'WATER', quantity: 2, price: 1},
       ])
       const quantity = 4
       const index = 0
-      const expectedItem = {name: 'WATER', quantity: 6, price: 10}
+      const expectedItem = {name: 'WATER', quantity: 6, price: 1}
 
       const stock = machine.stock.refill(quantity, index)
 
