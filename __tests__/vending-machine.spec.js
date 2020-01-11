a8ee0d54ac5480c1077cedf9f69f254ce59667ef
William L <w-lindvall@outlook.com>
2020-01-11 14:58:43 -0800

    fix test descriptions

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -1,157 +1,177 @@
 import VendingMachine from '../lib/vending-machine/index.js'
 
 describe('VendingMachine', () => {
-  describe('buy(valid index, valid payment)', () => {
-    const machine = new VendingMachine(
-      [{name: 'WATER', quantity: 2, price: 0.73}],
-      {
-        DOLLAR: 2,
-        TWO_DOLLAR: 1,
-        QUARTER: 1,
-      },
-    )
-    const purchase = machine.buy(0, {TWO_DOLLAR: 1})
+  describe('.buy()', () => {
+    describe('when called w valid params', () => {
+      const machine = new VendingMachine(
+        [{name: 'WATER', quantity: 2, price: 0.73}],
+        {
+          DOLLAR: 2,
+          TWO_DOLLAR: 1,
+          QUARTER: 1,
+        },
+      )
+      const purchase = machine.buy(0, {TWO_DOLLAR: 1})
 
-    it('returns purchased item name', () => {
-      expect(purchase[0]).toEqual('WATER')
+      it('returns purchased item name', () => {
+        expect(purchase[0]).toEqual('WATER')
+      })
+      it('returns change in coins', () => {
+        expect(purchase[1]).toEqual({DOLLAR: 1, QUARTER: 1})
+      })
+      it('returns updated stock', () => {
+        expect(purchase[2]).toEqual([
+          {name: 'WATER', quantity: 1, price: 0.73},
+        ])
+      })
     })
-    it('returns change in coins', () => {
-      expect(purchase[1]).toEqual({DOLLAR: 1, QUARTER: 1})
+
+    describe('when passed invalid index', () => {
+      const machine = new VendingMachine(
+        [{name: 'WATER', quantity: 2, price: 0.73}],
+        {
+          DOLLAR: 2,
+          TWO_DOLLAR: 1,
+          QUARTER: 1,
+        },
+      )
+
+      it(`throws 'OUT_OF_STOCK' error`, () => {
+        try {
+          machine.buy(1, {QUARTER: 1})
+        } catch (e) {
+          expect(e.message).toEqual('OUT_OF_STOCK')
+        }
+      })
     })
-    it('returns updated stock', () => {
-      expect(purchase[2]).toEqual([
-        {name: 'WATER', quantity: 1, price: 0.73},
-      ])
+
+    describe('when payment too low', () => {
+      const machine = new VendingMachine(
+        [{name: 'WATER', quantity: 2, price: 0.73}],
+        {
+          DOLLAR: 2,
+          TWO_DOLLAR: 1,
+          QUARTER: 1,
+        },
+      )
+
+      it(`throws 'LOW_PAYMENT' error`, () => {
+        try {
+          machine.buy(0, {QUARTER: 1})
+        } catch (e) {
+          expect(e.message).toEqual('LOW_PAYMENT')
+        }
+      })
     })
-  })
 
-  describe('buy(invalid index, valid payment)', () => {
-    const machine = new VendingMachine(
-      [{name: 'WATER', quantity: 2, price: 0.73}],
-      {
-        DOLLAR: 2,
-        TWO_DOLLAR: 1,
-        QUARTER: 1,
-      },
-    )
+    describe('when coin stock low', () => {
+      const machine = new VendingMachine(
+        [{name: 'BANANA', quantity: 2, price: 0.3}],
+        {
+          TWO_DOLLAR: 1,
+          DOLLAR: 2,
+        },
+      )
 
-    it(`throws 'OUT_OF_STOCK' error`, () => {
-      try {
-        machine.buy(1, {QUARTER: 1})
-      } catch (e) {
-        expect(e.message).toEqual('OUT_OF_STOCK')
-      }
+      it(`throws 'COINS_OUT_OF_STOCK' error`, () => {
+        try {
+          machine.buy(0, {QUARTER: 2})
+        } catch (e) {
+          expect(e.message).toEqual('COINS_OUT_OF_STOCK')
+        }
+      })
     })
-  })
 
-  describe('buy(valid index, valid payment too low)', () => {
-    const machine = new VendingMachine(
-      [{name: 'WATER', quantity: 2, price: 0.73}],
-      {
-        DOLLAR: 2,
-        TWO_DOLLAR: 1,
-        QUARTER: 1,
-      },
-    )
+    describe('when item out of stock', () => {
+      const machine = new VendingMachine(
+        [
+          {name: 'WATER', quantity: 2, price: 0.73},
+          {name: 'BANANA', quantity: 0, price: 0.3},
+        ],
+        {
+          DOLLAR: 2,
+          TWO_DOLLAR: 1,
+          QUARTER: 1,
+        },
+      )
 
-    it(`throws 'LOW_PAYMENT' error`, () => {
-      try {
-        machine.buy(0, {QUARTER: 1})
-      } catch (e) {
-        expect(e.message).toEqual('LOW_PAYMENT')
-      }
-    })
-  })
-
-  describe('buy(valid index, valid payment stock low)', () => {
-    const machine = new VendingMachine(
-      [{name: 'BANANA', quantity: 2, price: 0.3}],
-      {
-        TWO_DOLLAR: 1,
-        DOLLAR: 2,
-      },
-    )
-
-    it(`throws 'COINS_OUT_OF_STOCK' error`, () => {
-      try {
-        machine.buy(0, {QUARTER: 2})
-      } catch (e) {
-        expect(e.message).toEqual('COINS_OUT_OF_STOCK')
-      }
-    })
-  })
-
-  describe('buy(valid index out of stock, valid payment)', () => {
-    const machine = new VendingMachine(
-      [
-        {name: 'WATER', quantity: 2, price: 0.73},
-        {name: 'BANANA', quantity: 0, price: 0.3},
-      ],
-      {
-        DOLLAR: 2,
-        TWO_DOLLAR: 1,
-        QUARTER: 1,
-      },
-    )
-
-    it(`throws 'OUT_OF_STOCK' error`, () => {
-      try {
-        machine.buy(1, {DOLLAR: 1})
-      } catch (e) {
-        expect(e.message).toEqual('OUT_OF_STOCK')
-      }
-    })
-  })
-
-  describe('stock.get()', () => {
-    it('returns array of stocked items', () => {
-      const stock = [
-        {name: 'WATER', quantity: 2, price: 0.73},
-        {name: 'BANANA', quantity: 0, price: 0.3},
-      ]
-      expect(new VendingMachine(stock).stock.get()).toEqual(stock)
-    })
-    it('returns empty array if stock empty', () => {
-      expect(new VendingMachine().stock.get()).toEqual([])
+      it(`throws 'OUT_OF_STOCK' error`, () => {
+        try {
+          machine.buy(1, {DOLLAR: 1})
+        } catch (e) {
+          expect(e.message).toEqual('OUT_OF_STOCK')
+        }
+      })
     })
   })
 
-  describe('stock.refill(valid quantity, valid index, no item obj)', () => {
-    it('returns updated stock', () => {
-      const machine = new VendingMachine([
-        {name: 'WATER', quantity: 2, price: 1},
-      ])
-      const quantity = 4
-      const index = 0
-      const expectedItem = {name: 'WATER', quantity: 6, price: 1}
+  describe('stock', () => {
+    describe('.get()', () => {
+      describe('when items stocked', () => {
+        it('returns array of stocked items', () => {
+          const stock = [
+            {name: 'WATER', quantity: 2, price: 0.73},
+            {name: 'BANANA', quantity: 0, price: 0.3},
+          ]
+          expect(new VendingMachine(stock).stock.get()).toEqual(stock)
+        })
+      })
+      describe('when stock empty', () => {
+        it('returns empty array if stock empty', () => {
+          expect(new VendingMachine().stock.get()).toEqual([])
+        })
+      })
+    })
 
-      const stock = machine.stock.refill(quantity, index)
+    describe('.refill()', () => {
+      describe('when passed valid params', () => {
+        it('returns updated stock', () => {
+          const machine = new VendingMachine([
+            {name: 'WATER', quantity: 2, price: 1},
+          ])
+          const quantity = 4
+          const index = 0
+          const expectedItem = {
+            name: 'WATER',
+            quantity: 6,
+            price: 1,
+          }
 
-      expect(stock[index]).toEqual(expectedItem)
+          const stock = machine.stock.refill(quantity, index)
+
+          expect(stock[index]).toEqual(expectedItem)
+        })
+      })
     })
   })
 
-  describe('coins.get()', () => {
-    it('returns change', () => {
-      const coins = {
-        DOLLAR: 2,
-        TWO_DOLLAR: 1,
-      }
-      const machine = new VendingMachine([], coins)
+  describe('coins', () => {
+    describe('.get()', () => {
+      describe('when coins in stock', () => {
+        it('returns change', () => {
+          const coins = {
+            DOLLAR: 2,
+            TWO_DOLLAR: 1,
+          }
+          const machine = new VendingMachine([], coins)
 
-      expect(machine.coins.get()).toEqual(coins)
+          expect(machine.coins.get()).toEqual(coins)
+        })
+      })
     })
-  })
 
-  describe('coins.refill(valid refill obj)', () => {
-    it('returns updated total change', () => {
-      const machine = new VendingMachine()
-      const coins = {NICKEL: 4, DIME: 10}
-      const expectedTotal = 1.2
+    describe('.refill()', () => {
+      describe('when passed valid coins obj', () => {
+        it('returns updated total change', () => {
+          const machine = new VendingMachine()
+          const coins = {NICKEL: 4, DIME: 10}
+          const expectedTotal = 1.2
 
-      const updatedTotal = machine.coins.refill(coins)
+          const updatedTotal = machine.coins.refill(coins)
 
-      expect(updatedTotal).toEqual(expectedTotal)
+          expect(updatedTotal).toEqual(expectedTotal)
+        })
+      })
     })
   })
 })
